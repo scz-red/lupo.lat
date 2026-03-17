c7441200b70bb0161df984d3a57cbc4c0c536fa0
scz-red <enricox@live.it>
2026-03-17 03:29:42 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,72 @@
+const CACHE_NAME = 'lupo-v3';
+const OFFLINE_URL = '/offline.html';
+
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  OFFLINE_URL,
+  '/css/styles.css',
+  '/css/carousel.css',
+  '/js/calculadora.js',
+  '/js/carousel.js',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png',
+  '/icons/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const request = event.request;
+
+  if (request.method !== 'GET') return;
+
+  if (request.mode === 'navigate') {
+    event.respondWith(
+      fetch(request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(request, copy));
+          return response;
+        })
+        .catch(async () => {
+          const cached = await caches.match(request);
+          return cached || caches.match(OFFLINE_URL);
+        })
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then((cached) => {
+      return (
+        cached ||
+        fetch(request).then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(request, copy));
+          return response;
+        })
+      );
+    })
+  );
+});
