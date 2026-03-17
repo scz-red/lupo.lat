23181f46f0b0c7a0db28c1158479fc618edbc788
scz-red <enricox@live.it>
2026-03-17 04:24:39 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,72 +1,27 @@
-const CACHE_NAME = 'lupo-v3';
-const OFFLINE_URL = '/offline.html';
+const CACHE_NAME = "lupo-cache-v1";
 
-const ASSETS_TO_CACHE = [
-  '/',
-  '/index.html',
-  OFFLINE_URL,
-  '/css/styles.css',
-  '/css/carousel.css',
-  '/js/calculadora.js',
-  '/js/carousel.js',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png',
-  '/icons/manifest.json'
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/css/styles.css",
+  "/css/carousel.css",
+  "/js/calculadora.js",
+  "/js/carousel.js",
+  "/offline.html"
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
-  self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', (event) => {
-  const request = event.request;
-
-  if (request.method !== 'GET') return;
-
-  if (request.mode === 'navigate') {
-    event.respondWith(
-      fetch(request)
-        .then((response) => {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then((cache) => cache.put(request, copy));
-          return response;
-        })
-        .catch(async () => {
-          const cached = await caches.match(request);
-          return cached || caches.match(OFFLINE_URL);
-        })
-    );
-    return;
-  }
-
+self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(request).then((cached) => {
-      return (
-        cached ||
-        fetch(request).then((response) => {
-          const copy = response.clone();
-          caches.open(CACHE_NAME).then((cache) => cache.put(request, copy));
-          return response;
-        })
-      );
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request).catch(() => caches.match("/offline.html"));
     })
   );
 });
